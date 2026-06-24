f70b56d43b11d5043805de6448438e9fe8f47915
Jose Francisco <francesneto6@gmail.com>
2026-06-24 12:36:18 -0300

    feat: add initial commit spring initializr

---
+++ src/main/java/com/neto/CycleRush/CycleRushApplication.java
@@ -0,0 +1,13 @@
+package com.neto.CycleRush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleRushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycleRushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neto/CycleRush/CycleRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neto.CycleRush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleRushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
